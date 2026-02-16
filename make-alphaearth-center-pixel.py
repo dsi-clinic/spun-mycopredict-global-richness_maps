de5801e0cf4bd2e4044f4c9509a407b18c6e1932
Jim Pivarski <jpivarski@gmail.com>
2026-02-16 17:08:01 -0600

    make a training file using AlphaEarth values averaged over weighted balls (sigma=3 pixels)

--- make-alphaearth-center-pixel.py
+++ make-alphaearth-center-pixel.py
@@ -5,26 +5,36 @@ import pandas as pd
 
 DIRECTORY = pathlib.Path("~/Box/dsi-core/11th-hour/spun/patches/").expanduser()
 
+y, x = np.ogrid[:25, :25]
+gaussian = np.exp(-((x-12)**2 + (y-12)**2) / (2.0*3**2))
+gaussian /= gaussian.sum()
+
 am_alpha23_file = np.load(DIRECTORY / "alpha_earth_AM_25x25_2017.npz")
 am_alpha23_patches = am_alpha23_file["patches"]
 am_alpha23_ids = am_alpha23_file["ids"]
 am_alpha23_band_names = am_alpha23_file["band_names"]
+
+am_alpha23_patches = (
+    am_alpha23_patches * gaussian[np.newaxis, :, :, np.newaxis]
+).sum(axis=(1,2))
+
 am_alpha23 = pd.DataFrame(
     {"sample_id": am_alpha23_ids}
-    | {
-        x: am_alpha23_patches[:, 12, 12, i] for i, x in enumerate(am_alpha23_band_names)
-    },
+    | {x: am_alpha23_patches[:, i] for i, x in enumerate(am_alpha23_band_names)},
 )
 
 em_alpha23_file = np.load(DIRECTORY / "alpha_earth_EM_25x25_2017.npz")
 em_alpha23_patches = em_alpha23_file["patches"]
 em_alpha23_ids = em_alpha23_file["ids"]
 em_alpha23_band_names = em_alpha23_file["band_names"]
+
+em_alpha23_patches = (
+    em_alpha23_patches * gaussian[np.newaxis, :, :, np.newaxis]
+).sum(axis=(1,2))
+
 em_alpha23 = pd.DataFrame(
     {"sample_id": em_alpha23_ids}
-    | {
-        x: em_alpha23_patches[:, 12, 12, i] for i, x in enumerate(em_alpha23_band_names)
-    },
+    | {x: em_alpha23_patches[:, i] for i, x in enumerate(em_alpha23_band_names)},
 )
 
 am = pd.read_csv(
@@ -40,5 +50,7 @@ am_alpha = am_alpha[["sample_id"] + list(am_alpha23_band_names) + list(am.column
 ecm_alpha = ecm.merge(em_alpha23, on="sample_id")
 ecm_alpha = ecm_alpha[["sample_id"] + list(em_alpha23_band_names) + list(ecm.columns[25:])]
 
-am_alpha.to_csv("data/20260123_arbuscular_mycorrhizal_only_alphaearth_center.csv", index=False)
-ecm_alpha.to_csv("data/20260123_ectomycorrhizal_only_alphaearth_center.csv", index=False)
+am_alpha.to_csv(
+    "data/20260123_arbuscular_mycorrhizal_only_alphaearth_ball.csv", index=False
+)
+ecm_alpha.to_csv("data/20260123_ectomycorrhizal_only_alphaearth_ball.csv", index=False)
