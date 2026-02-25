7621dc64ca05041b4b5883b00d360ff27fb2fd5e
Jim Pivarski <jpivarski@gmail.com>
2026-02-25 14:03:25 -0600

    fix input file name

--- permute-inputs.py
+++ permute-inputs.py
@@ -10,7 +10,7 @@ EARTH_RADIUS_KM = 6371.0
 DISTANCE_CUT, AM_OUTPUT, EM_OUTPUT = sys.argv[1:]
 DISTANCE_CUT = float(DISTANCE_CUT)
 
-am = pd.read_csv("data/20260122_arbuscular_mycorrhizal_richness_training_data.csv")
+am = pd.read_csv("data/20260123_arbuscular_mycorrhizal_only_alphaearth_center.csv")
 if "longitude" in am.columns:
     longitude = am["longitude"]
     latitude = am["latitude"]
@@ -30,7 +30,7 @@ am.to_csv(AM_OUTPUT, index=False)
 
 del am, longitude, latitude, am_distances, original_features, random_features, i, j
 
-em = pd.read_csv("data/20260122_ectomycorrhizal_richness_training_data.csv")
+em = pd.read_csv("data/20260123_ectomycorrhizal_only_alphaearth_center.csv")
 if "longitude" in em.columns:
     longitude = em["longitude"]
     latitude = em["latitude"]
